8277b99fe553b720d375e29f0f3bdbf086cde671
Andrey Mukhin <mukhin.av.1997@gmail.com>
2021-05-04 22:24:45 +0300

    Add stack implementation on Python.

---
+++ leetcode_tasks/2.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        carry = 0
+        res_list_node = ListNode(0)
+        curr = res_list_node
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            carry, value = divmod(carry, 10)
+            curr.next = curr = ListNode(value)
+        return res_list_node.next
